85c8021d4d6e9f5130142e0aa50a3b4fcc2b6699
eshikvtumane <pavel.tolstolytko@yandex.ru>
2019-04-27 21:11:36 +1000

    Change parser

--- deklarator.py
+++ deklarator.py
@@ -20,21 +20,21 @@ class Country(Enum):
     def get_country_value_by_name(cls, name):
         name = name.decode('utf-8').lower()
 
-        if u'беларусь' == name:
+        if u'беларусь' in name:
             return cls.BELARUS.value
-        elif u'грузия' == name:
+        elif u'грузия' in name:
             return cls.GEORGIA.value
-        elif u'казахстан' == name:
+        elif u'казахстан' in name:
             return cls.KAZAKHSTAN.value
-        elif u'литва' == name:
+        elif u'литва' in name:
             return cls.LITHUANIA.value
-        elif u'португалия' == name:
+        elif u'португалия' in name:
             return cls.PORTUGAL.value
-        elif u'россия' == name:
+        elif u'россия' in name:
             return cls.RUSSIA.value
-        elif u'сша' == name:
+        elif u'сша' in name:
             return cls.USA.value
-        elif u'украина' == name:
+        elif u'украина' in name:
             return cls.UKRAINE.value
         else:
             return cls.NOT_DETERMENATED.value
@@ -48,14 +48,21 @@ class OwnershipType(Enum):
 
     @classmethod
     def get_ownership_value_by_name(cls, name):
-        if 'индивидуальная' in name:
+        if '(собственность)' in name and 'дол' in name:
+            return cls.PARTIAL.value
+        elif '(собственность)' in name:
+            return cls.INDIVIDUAL.value
+        elif 'индивидуальная' in name:
             return cls.INDIVIDUAL.value
         elif 'совместная' in name:
             return cls.INDIVIDUAL.value
         elif 'долевая' in name:
             return cls.PARTIAL.value
+        elif 'доли' in name:
+            return cls.PARTIAL.value
         else:
-            return cls.NOT_DETERMENATED.value
+            return cls.INDIVIDUAL.value
+            # return cls.NOT_DETERMENATED.value
 
 
 class ObjectType(Enum):
@@ -83,6 +90,8 @@ class ObjectType(Enum):
         name = name.decode('utf-8').lower()
 
         if u'садовый участок' in name:
+            return cls.GARDEN.value
+        elif u'садовый' in name:
             return cls.GARDEN.value
         elif u'нежилой дом' in name:
             return cls.NOT_LIVING_HOUSE.value
@@ -120,7 +129,6 @@ class ObjectType(Enum):
             return cls.NOT_DETERMENATED.value
 
 
-
 class RelationType(Enum):
     NOT_DETERMENATED = 0
     HUSBAND = 1
@@ -133,7 +141,7 @@ class RelationType(Enum):
             return cls.WIFE.value
         elif u'супруг' in name:
             return cls.HUSBAND.value
-        elif u'ребенок' in name:
+        elif u'несовершеннолетн' in name or u'ребенок' in name:
             return cls.CHILD.value
         else:
             return None
@@ -187,18 +195,24 @@ class DeklaratorXML(object):
             relative_of = self.create_tag_with_text('relativeOf', self._id)
 
         person_id = self.create_tag_with_text('id', person_id_value)
-        # person_id = etree.Element('id')
-        # person_id.text = unicode(str(person_id_value), "utf-8")
 
         if person.objects_owner is [] and person.objects_not_owner is None:
             realties_tags = self.create_tag_with_nil('realties')
-            # realties_tags = etree.Element('realties', attrib={
-            #     etree.QName("http://www.w3.org/2001/XMLSchema-instance", "nil"): 'true',
-            # })
         else:
             realties_tags = etree.Element('realties')
             self.get_realties(realties_tags, person.objects_owner, 1)
             self.get_realties(realties_tags, person.objects_not_owner, 2)
+
+        if person.machines == []:
+            transports_tags = self.create_tag_with_nil('transports')
+        else:
+            transports_tags = etree.Element('transports')
+            for machine in person.machines:
+                transport_tags = etree.Element('transport')
+                transport_name_tags = self.create_tag_with_text('transportName', machine)
+
+                transport_tags.append(transport_name_tags)
+                transports_tags.append(transport_tags)
 
         person_tag.append(person_id)
         person_tag.append(name)
@@ -209,6 +223,7 @@ class DeklaratorXML(object):
         person_tag.append(relative_of)
         person_tag.append(relative_type)
         person_tag.append(realties_tags)
+        person_tag.append(transports_tags)
 
         return person_tag
 
@@ -216,46 +231,28 @@ class DeklaratorXML(object):
         for realty in realties:
             realty_tag = etree.Element('realty')
             realty_type_tag = self.create_tag_with_text('realtyType', realty_type)
-            # realty_type_tag = etree.Element('realtyType')
-            # realty_type_tag.text = unicode(str(realty_type), "utf-8")
 
             realty_name_tag = self.create_tag_with_text('RealtyName', realty[0])
-            # realty_name_tag = etree.Element('RealtyName')
-            # realty_name_tag.text = unicode(str(realty[0]), "utf-8")
 
             if realty_type == 1:
                 ownership_type = OwnershipType.get_ownership_value_by_name(realty[0])
                 ownership_type_tag = self.create_tag_with_text('ownershipType', ownership_type)
-                # ownership_type_tag = etree.Element('ownershipType')
-                # ownership_type_tag.text = unicode(str(ownership_type), "utf-8")
 
                 if ownership_type == OwnershipType.PARTIAL.value:
-                    # ownership_part_tag = etree.Element('ownershipPart')
-                    one, two = re.search(r'[0-9]+/[0-9]+$', realty[0]).group(0).split('/')
+                    one, two = re.search(r'[0-9]+/[0-9]+', realty[0]).group(0).split('/')
                     ownership_part = float(one) / float(two)
-                    # ownership_part_tag.text = unicode(str(ownership_part), "utf-8")
                     ownership_part_tag = self.create_tag_with_text('ownershipPart', ownership_part)
                 else:
                     ownership_part_tag = self.create_tag_with_nil('ownershipPart')
-                    # ownership_part_tag = etree.Element('ownershipPart', attrib={
-                    #     etree.QName("http://www.w3.org/2001/XMLSchema-instance", "nil"): 'true',
-                    # })
                 realty_tag.append(ownership_type_tag)
                 realty_tag.append(ownership_part_tag)
 
             realty_object_tag = self.create_tag_with_text('objectType', ObjectType.get_value_by_name(realty[0]))
-            # realty_object_tag = etree.Element('objectType')
-            # realty_object_tag.text = unicode(str(ObjectType.get_value_by_name(realty[0])), "utf-8")
 
             square_tag = self.create_tag_with_text('square', realty[1])
-            # square_tag = etree.Element('square')
-            # square_tag.text = unicode(str(realty[1]), "utf-8")
 
             country_value = Country.get_country_value_by_name(realty[2])
             country_tag = self.create_tag_with_text('country', country_value)
-            # country_tag = etree.Element('country')
-            # country_value = Country.get_country_value_by_name(realty[2])
-            # country_tag.text = unicode(str(country_value), "utf-8")
 
             realty_tag.append(realty_name_tag)
             realty_tag.append(realty_type_tag)
@@ -279,29 +276,3 @@ class DeklaratorXML(object):
             etree.QName("http://www.w3.org/2001/XMLSchema-instance", "nil"): 'true',
         })
         return tag
-
-if __name__ == '__main__':
-    # create XML
-    # root = etree.Element('root')
-
-    persons = etree.Element('persons', attrib={
-        etree.QName("http://www.w3.org/2001/XMLSchema-instance",
-                    "noNamespaceSchemaLocation"): 'declarationXMLtemplate_Schema _transport_merged.xsd',
-    })
-
-    person = etree.Element('person')
-
-    person_id = etree.Element('id')
-    person_id.text = '1'
-
-    person_name = etree.Element('name')
-    person_name.text = '1'
-
-    person_name = etree.Element('name')
-    person_name.text = '1'
-
-    person.append(person_id)
-    persons.append(person)
-
-    s = etree.tostring(persons, pretty_print=True, xml_declaration=True, encoding='utf-8')
-    print(s)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 import docx
 
+from auto import Auto
 from deklarator import DeklaratorXML
 
 
@@ -37,11 +38,17 @@ class Depute(object):
         if machine != '' and machine not in self._machines_list:
             self._machines_list.append(machine)
             type_machine = ''
+            machines = []
             for machine in machine.split(';'):
+                machines += machine.split('\n')
+
+            for machine in machines:
                 if ':' in machine:
                     type_machine = machine.split(':')[0]
 
                     if 'автомобили легковые' in type_machine:
+                        type_machine = 'а/м легковой'
+                    elif 'автомобиль легковой' in type_machine:
                         type_machine = 'а/м легковой'
                     elif 'автомобили грузовые' in type_machine:
                         type_machine = 'а/м грузовой'
@@ -49,13 +56,47 @@ class Depute(object):
                         type_machine = 'Моторная лодка'
                     elif 'иные транспортные средства' in type_machine:
                         type_machine = type_machine.replace('иные транспортные средства ', '')
-                    machine = machine.split(':')[1]
+                    elif 'водный транспорт' in type_machine:
+                        type_machine = type_machine.replace('водный транспорт', '')
+                    elif 'мототранспортные средства' in type_machine:
+                        type_machine = type_machine.replace('мототранспортное средство', '')
+                    elif Auto.is_auto(type_machine):
+                        machine = type_machine
+                        type_machine = ''
+                    machine = machine.split(':')[1].strip()
 
-                if machine != '':
-                    self.machines.append(('%s %s' % (type_machine, machine)).strip())
+                    if machine:
+                        self.machines.append(('%s %s' % (type_machine, machine)).strip().replace('  ', ' '))
+                # elif '' == machine:
+                #     type_machine = ''
+                else:
+                    if machine:
+                        if 'водный транспорт' == machine:
+                            type_machine = machine
+                        elif 'автомобили легковые' == machine:
+                            type_machine = 'а/м легковой'
+                        elif 'автомобиль легковой' == machine:
+                            type_machine = 'а/м легковой'
+                        elif 'мототранспортное средство мотоцикл' == machine:
+                            type_machine = 'мототранспортное средство мотоцикл'
+                        elif Auto.is_auto(type_machine):
+                            machine = type_machine
+                            type_machine = ''
+                            self.machines.append(('%s' % (machine)).strip().replace('  ', ' '))
+                        else:
+                            self.machines.append(('%s %s' % (type_machine, machine)).strip().replace('  ', ' '))
+                    # self.machines.append(machine.strip())
+
 
     def clear_number(self, number):
         return number.replace(' ', '').replace(' ', '').replace(',', '.')
+
+    def write_owner_for_old_files(self, owner, square, country):
+        if 'собственность' in owner or 'совместная собственность' in owner \
+                or 'долевая собственность' in owner or 'доли' in owner:
+            self.set_object_owner(owner, square, country)
+        else:
+            self.set_object_not_owner(owner, square, country)
 
     def __str__(self):
         return self.name
@@ -65,28 +106,79 @@ class Depute(object):
 
 
 if __name__ == '__main__':
-    doc = docx.Document('2017_Deputaty.docx')
+    # doc = docx.Document('2015_Sotrudniki_apparata.docx')
+    # doc = docx.Document('2015_Deputaty_(utochnionnye).docx')
+    # doc = docx.Document('2017_Deputaty.docx')
+    filename = '2013_Deputaty.docx'
+    filename = '2013_Sotrudniki_apparata.docx'
+    output_filename = '%s.xml' % filename.split('.')[0].lower()
+    doc = docx.Document(filename)
 
     deputies = []
 
-    for i, row in enumerate(doc.tables[1].rows[1:]):
-        data = [cell.text.encode('utf-8').replace('\n', ' ').replace('  ', ' ').rstrip() for cell in row.cells]
+    if len(doc.tables) > 1:
+        table = doc.tables[1]
+        row_start = 1
+    else:
+        table = doc.tables[0]
+        row_start = 2
+
+    for i, row in enumerate(table.rows[row_start:]):
+        # print('loop')
+        data = []
+        for idx, cell in enumerate(row.cells):
+            if len(row.cells) == 7 and idx == 6:
+                data.append(cell.text.encode('utf-8').replace('  ', ' ').rstrip())
+            elif ('2012' in filename and len(row.cells) == 7):
+                if idx == 3 or idx == 4 or idx == 5:
+                    data.append(cell.text.encode('utf-8'))
+                else:
+                    data.append(cell.text.encode('utf-8').replace('\n', ' ').replace('  ', ' ').rstrip())
+            elif idx == 10:
+                data.append(cell.text.encode('utf-8').replace('  ', ' ').rstrip())
+            else:
+                data.append(cell.text.encode('utf-8').replace('\n', ' ').replace('  ', ' ').rstrip())
+        # data = [cell.text.encode('utf-8').replace('\n', ' ').replace('  ', ' ').rstrip() for cell in row.cells]
         if len(deputies) > 0 and data[0] == deputies[-1].name or data[0] == '':
             depute = deputies[-1]
         else:
+            # if i > 0:
+            #     break
+            print(data[0])
             depute = Depute()
             depute.set_name(data[0])
             depute.position = data[1]
             depute.set_salary(data[2])
             deputies.append(depute)
 
-        depute.set_object_owner(data[3], data[4], data[5])
-        depute.set_object_not_owner(data[6], data[7], data[8])
-        depute.set_machine(data[9])
+        if len(data) == 11:
+            depute.set_object_owner(data[3], data[4], data[5])
+            depute.set_object_not_owner(data[6], data[7], data[8])
+            depute.set_machine(data[9])
+        else:
+            # if 'аренда' in data[3]:
+            #     data[3] = data[3].replace('аренда', '(пользование)')
+            if '2012' in filename:
+                objects_list = [o.replace(',', '.') for o in data[3].split('\n') if o != '']
+                square_list = [s.replace(',', '.') for s in data[4].split('\n') if s != '']
+                country_list = [c for c in data[5].split('\n') if c != '']
 
+                for idx in xrange(0, len(objects_list)):
+                    square = square_list[idx]
+                    country = country_list[idx]
+                    depute.write_owner_for_old_files(objects_list[idx], square, country)
+
+            else:
+                depute.write_owner_for_old_files(data[3], data[4], data[5])
+            depute.set_machine(data[6])
+
+    print('Create xml ...')
     deklarator = DeklaratorXML()
 
     deklarator.add_info(deputies)
 
-    with open('2017_deputaty.xml', 'wb') as f:
+    # with open('2015_sotrudniki_apparata.xml', 'wb') as f:
+    # with open('2015_deputaty.xml', 'wb') as f:
+    # with open('2015_sotrudniki_apparata.xml', 'wb') as f:
+    with open(output_filename, 'wb') as f:
         f.write(deklarator.get_xml())
